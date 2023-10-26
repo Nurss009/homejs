96397c284a7ab40584b548d4dfe3d36fce8bb3e3
Nursultan <tupoukidala77@gmail.com>
2023-10-26 21:02:44 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+// let a = 'hello'
+// let b = 'Ivan'
+// let c = a.slice(0,1).toUpperCase() + a.slice(1) + ' ' + b.slice(0,1).toLowerCase() + b.slice(1)
+// console.log(c)
+////
+// let a = 'lorem ipsum dolor'
+// let b = a.length
+// console.log(b)
+////
+// let a = 'lorem ipsum dolor sit amet'
+// let b = a.slice(18,21) + ' ' + a.slice(0,17) + a.slice(21)
+// console.log(b)
+////
+// let a = 'children'
+// let b = a.slice(0,7).toUpperCase()+a.slice(7)
+// console.log(b)
+////
+// let a = 'boss'
+// let b = a.slice(0,1) + a.slice(2) + a.slice(1,2)
+// console.log(b)
+////
+// let a = 'sunday'
+// let b = a.indexOf('d')
+// console.log(b)
